56a20a2e096c55fe7c5c7124e9e407b7410b54c0
Oli Evans <oli@zilla.org.uk>
2016-10-19 23:36:12 +0100

    Swatch style

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 
 You want to put color swatches in your markdown, but you can't be bothered to make or host the pngs.
 
-![Charcoal](https://swatches-lrwumaetdq.now.sh/?color=%2324364C)
-![Royal blue](https://swatches-lrwumaetdq.now.sh/?color=%232B60D5)
-![Ultramarine blue](https://swatches-lrwumaetdq.now.sh/?color=%23437AF4)
-![Bleu de France](https://swatches-lrwumaetdq.now.sh/?color=%234299FF)
+![Charcoal](https://swatches-lkzftbccpg.now.sh/?color=%2324364C)
+![Royal blue](https://swatches-lkzftbccpg.now.sh/?color=%232B60D5)
+![Ultramarine blue](https://swatches-lkzftbccpg.now.sh/?color=%23437AF4)
+![Bleu de France](https://swatches-lkzftbccpg.now.sh/?color=%234299FF)
 
 ```md
-![Charcoal](https://swatches-lrwumaetdq.now.sh/?color=%2324364C)
-![Royal blue](https://swatches-lrwumaetdq.now.sh/?color=%232B60D5)
-![Ultramarine blue](https://swatches-lrwumaetdq.now.sh/?color=%23437AF4)
-![Bleu de France](https://swatches-lrwumaetdq.now.sh/?color=%234299FF)
+![Charcoal](https://swatches-lkzftbccpg.now.sh/?color=%2324364C)
+![Royal blue](https://swatches-lkzftbccpg.now.sh/?color=%232B60D5)
+![Ultramarine blue](https://swatches-lkzftbccpg.now.sh/?color=%23437AF4)
+![Bleu de France](https://swatches-lkzftbccpg.now.sh/?color=%234299FF)
 ```
 
 Add your `color` at the end of the url, get back an svg square.
 
-https://swatches-lrwumaetdq.now.sh/?color=hotpink
+https://swatches-lkzftbccpg.now.sh/?color=hotpink
 
 For hex codes you have to url encode the `#` with `%23`.
 
--- index.js
+++ index.js
@@ -5,17 +5,25 @@ const PORT = process.env.PORT || 3000
 http.createServer((req, res) => {
   const query = url.parse(req.url, true).query
   console.log(query)
-  const width = query.width || query.w || query.size || query.s || '100'
-  const height = query.height || query.h || width
-  let color = query.color || query.c || '#2B60D5'
+  const width = Number(query.width || query.w || query.size || query.s || '150')
+  const outerWidth = width + 12
+  const height = Number(query.height || query.h || width)
+  const outerHeight = height + 50
+  const color = query.color || query.c || 'hotpink'
 
   res.setHeader('Content-Type', 'image/svg+xml')
   res.setHeader('ETag', width + height + color)
   res.setHeader('Cache-Control', 'max-age=86400')
   res.end(`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg width="${width}px" height="${height}px" viewBox="0 0 ${width} ${height}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <rect id="Rectangle" fill="${color}" x="0" y="0" width="${width}" height="${height}"></rect>
-</svg>`)
+  <svg width="${outerWidth}px" height="${outerHeight}px" viewBox="0 0 ${outerWidth} ${outerHeight}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <rect fill="#FFFFFF" x="0" y="0" width="${outerWidth}" height="${outerHeight}"></rect>
+    <rect fill="${color}" x="6" y="6" width="${width}" height="${height}"></rect>
+    <text font-family="-apple-system,BlinkMacSystemFont,'avenir next',avenir,helvetica, 'helvetica neue',ubuntu,roboto, noto,'segoe ui',arial,sans-serif" font-size="16" font-weight="bold" fill="#4A4A4A">
+      <tspan x="7" y="${height + 34}">${color}</tspan>
+    </text>
+  </svg>
+  `)
+
 }).listen(PORT)
 
 console.log('SWATCHES listening on %s', PORT)
